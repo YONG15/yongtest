6d0dc48be5880d38b2dd65d2d3f416fc16bd1eae
YONG15 <dyddnals@naver.com>
2023-02-14 17:33:23 +0900

    다익스트라

--- algorithm/02-14/다익스트라.cpp
+++ algorithm/02-14/다익스트라.cpp
@@ -281,6 +281,68 @@
 7 6 10
 6 2 10
 */
+// #include <iostream>
+// #include <queue>
+// #include <string>
+// using namespace std;
+// struct Edge {
+// 	int node; 
+// 	int cost; 
+// };
+// struct Node {
+// 	int node; // start ~~~~ > node
+// 	int accCost; // start~~~>node 까지의 누적비용 
+// };
+// struct cmp
+// {
+// 	bool operator() (Node& left, Node& right)
+// 	{
+// 		return left.accCost > right.accCost; 
+// 	}
+// };
+// int N, T; 
+// vector<Edge> adj[8]; 
+// void input() {
+// 	cin >> N >> T; 
+// 	for (int i = 0; i < T; i++)
+// 	{
+// 		int a, b, cost; 
+// 		cin >> a >> b >> cost; 
+// 		adj[a].push_back({ b,cost });
+// 	}
+// }
+// int main() {
+// 	freopen_s(new FILE*, "input.txt", "r", stdin);
+// 	input(); 
+// 	// 가짜 dijkstra 
+// 	priority_queue<Node, vector<Node>, cmp> pq; 
+// 	int processed[8] = { 0 }; 
+// 	int start = 1; 
+// 	pq.push({ start, 0}); // start, start~~>start비용
+// 	int dist[8] = { 0 }; 
+// 	while (!pq.empty())
+// 	{
+// 		Node now = pq.top(); pq.pop(); 
+// 		if (processed[now.node] == 1)continue; 
+// 		processed[now.node] = 1; 
+// 		dist[now.node] = now.accCost;
+// 		// proccess 
+// 		for (Edge& next : adj[now.node])
+// 		{
+// 			int newCost = now.accCost + next.cost; // start~~~~>now + 간선비용
+// 			pq.push({ next.node, newCost }); 
+// 		}
+// 	}
+// 	for (int node = 1; node <= 7; node++)
+// 	{
+// 		cout << start << "~~~~~~>" << node << "의 최소비용 : " << dist[node] << endl; 
+// 	}
+// 	return 0;
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//진짜다익스트라
 #include <iostream>
 #include <queue>
 #include <string>
@@ -311,31 +373,37 @@ void input() {
 		adj[a].push_back({ b,cost });
 	}
 }
+const int INF = 1e9; // 10억 
 int main() {
 	freopen_s(new FILE*, "input.txt", "r", stdin);
 	input(); 
-	// 가짜 dijkstra 
 	priority_queue<Node, vector<Node>, cmp> pq; 
-	int processed[8] = { 0 }; 
+	int dist[8];  // dist[A] : PQ에 있는 A의 제일 작은 누적비용 
+	for (int i = 1; i <= 7; i++) dist[i] = INF; 
+	int processed[8] = { 0 };
 	int start = 1; 
 	pq.push({ start, 0}); // start, start~~>start비용
-	int dist[8] = { 0 }; 
+	dist[start] = 0; 
 	while (!pq.empty())
 	{
 		Node now = pq.top(); pq.pop(); 
 		if (processed[now.node] == 1)continue; 
 		processed[now.node] = 1; 
-		dist[now.node] = now.accCost;
 		// proccess 
 		for (Edge& next : adj[now.node])
 		{
 			int newCost = now.accCost + next.cost; // start~~~~>now + 간선비용
-			pq.push({ next.node, newCost }); 
+			if (newCost < dist[next.node])
+			{
+				dist[next.node] = newCost; 
+				pq.push({ next.node, dist[next.node]});
+			}
 		}
 	}
 	for (int node = 1; node <= 7; node++)
 	{
 		cout << start << "~~~~~~>" << node << "의 최소비용 : " << dist[node] << endl; 
 	}
+	
 	return 0;
 }
