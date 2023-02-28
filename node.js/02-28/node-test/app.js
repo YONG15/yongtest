63bc74d9d60319bf0051b8e841e72d12ce1b70a4
YONG15 <dyddnals@naver.com>
2023-02-28 15:23:59 +0900

    nodejs

---
+++ node.js/02-28/node-test/app.js
@@ -0,0 +1,8 @@
+// const calc = require("./calc.js")
+
+// console.log(calc.add(1, 2));
+
+
+const {printHello} = require("./calc.js")
+
+console.log(printHello());
---
+++ node.js/02-28/node-test/calc.js
@@ -0,0 +1,18 @@
+const calc = {
+  add: function(a, b) {
+    return a+b;
+  },
+  substrat: function(a, b) {
+    return a-b;
+  },
+  multiply: function(a, b) {
+    return a*b;
+  },
+  divide: function(a, b) {
+    return a/b;
+  },
+  printHello: function() {
+    console.log("hello");
+  }
+};
+module.exports = calc;
