91e94cd38a24aa50c8f7df1dc39cf255ba35d046
YONG15 <dyddnals@naver.com>
2023-03-17 17:31:32 +0900

    네이버검색데이터 시각화 PJT

---
+++ vue_js/03-17/web_pjt4/frontend/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue</h1>
+    <LineChart />
+    <DataSendForm />
+  </div>
+</template>
+
+<script>
+import { dataLab } from "./utils/axios";
+import { mapState, mapMutations } from "vuex";
+import LineChart from "./components/LineChart";
+import DataSendForm from "./components/DataSendForm.vue";
+export default {
+  name: "App",
+  components: { LineChart, DataSendForm },
+  async created() {
+    try {
+      const response = await dataLab.get();
+      const first = response.data[0];
+      const labels = first.data.map((item) => item.period);
+      const datasets = response.data.map((item) => {
+        return {
+          label: item.title,
+          borderColor: this.makeColor(),
+          data: item.data.map((item) => item.ratio),
+          tension: 0.3,
+        };
+      });
+      const chartData = {
+        labels: labels,
+        datasets: datasets,
+      };
+      this.CHANGE_CHART_DATA(chartData);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  methods: {
+    ...mapMutations(["CHANGE_CHART_DATA"]),
+    makeColor() {
+      return "#" + Math.round(Math.random() * 0xffffff).toString(16);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
